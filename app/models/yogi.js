ac35e0738362f6779e5aa5011af5532a8bbe4b15
matt-winzer <matt.winzer@gmail.com>
2017-03-18 13:23:18 -0600

    yogi model working without sequence relationship

---
+++ app/models/yogi.js
@@ -0,0 +1,11 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  username: DS.attr(),
+  email: DS.attr(),
+  password: DS.attr(),
+  bio: DS.attr(),
+  profilePic: DS.attr(),
+  dateCreated: DS.attr('date'),
+  // sequences: DS.hasMany('sequence', {async: true})
+});
