fcd21d75c4c393c2e291760a0bf030d2d1dfd50c
matt-winzer <matt.winzer@gmail.com>
2017-03-19 14:22:18 -0600

    belongsTo and HasMany both working for yogis & sequences

--- app/routes/yogi.js
+++ app/routes/yogi.js
@@ -4,4 +4,12 @@ export default Ember.Route.extend({
   model(params){
   return this.get('store').findRecord('yogi', params.id);
 }
+
+// model(params) {
+//   return Ember.RSVP.hash({
+//     yogi: this.get('store').findRecord('yogi', params.id),
+//     sequence: this.get('store').findRecord('sequence', params.id)
+//   });
+// }
+
 });
