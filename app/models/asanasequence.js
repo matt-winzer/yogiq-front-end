bb306b28eab485db8adbe18d03f6b7050765631b
matt-winzer <matt.winzer@gmail.com>
2017-03-20 15:24:53 -0600

    relationship between asanas and asanasequences working

--- app/models/asanasequence.js
+++ app/models/asanasequence.js
@@ -2,6 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   asana: DS.belongsTo('asana', {async: true}),
-  sequence: DS.belongsTo('sequence', {async: true}),
-  sortOrder: DS.attr('number')
+  // sequence: DS.belongsTo('sequence', {async: true}),
+  sortOrder: DS.attr()
 });
