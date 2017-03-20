3e0ae347f305c8974aff1421734aa31d0b372ee2
matt-winzer <matt.winzer@gmail.com>
2017-03-19 20:49:38 -0600

    asanas displaying with responsive image, description, duration

--- app/controllers/asanas.js
+++ app/controllers/asanas.js
@@ -2,5 +2,5 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   sortProps: ['name'],
-  sortedAsanas: Ember.computed.sort('model', 'sortProps')
+  asanasByName: Ember.computed.sort('model', 'sortProps')
 });
