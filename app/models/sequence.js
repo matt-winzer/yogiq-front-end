4f86cd7564f30208ad0e02d6e95b4c1d79dc52f1
matt-winzer <matt.winzer@gmail.com>
2017-03-20 16:26:39 -0600

    relationship between sequences and asanasequences working

--- app/models/sequence.js
+++ app/models/sequence.js
@@ -1,5 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  yogi: DS.belongsTo("yogi", {async: true})
+  yogi: DS.belongsTo("yogi", {async: true}),
+  asanasequences: DS.hasMany('asanasequence', {async: true})
 });
--- app/serializers/asanasequence.js
+++ app/serializers/asanasequence.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.RESTSerializer.extend({
   attrs: {
-    asana: {key: 'asanaID'}
-    // sequence: {key: 'sequenceID'}
+    asana: {key: 'asanaID'},
+    sequence: {key: 'sequenceID'}
   }
 });
