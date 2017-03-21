ed66d5356ef0cff75a2e96f0c60fcf9eb5687e13
matt-winzer <matt.winzer@gmail.com>
2017-03-20 21:17:15 -0600

    asanasequnces displaying on sequence tab of profile

--- app/models/asanasequence.js
+++ app/models/asanasequence.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   asana: DS.belongsTo('asana', {async: true}),
   sequence: DS.belongsTo('sequence', {async: true}),
-  sortOrder: DS.attr()
+  sortOrder: DS.attr('number')
 });
