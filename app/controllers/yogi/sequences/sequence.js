a931900b28e7ce92a6862d301d98836ff032da27
matt-winzer <matt.winzer@gmail.com>
2017-03-22 00:11:01 -0600

    computer asansequences and related properties being collected when edit button clicked

--- app/controllers/yogi/sequences/sequence.js
+++ app/controllers/yogi/sequences/sequence.js
@@ -1,5 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  hello: 'hello'
+  editMode: false,
+  currentPoses: [],
+
+  actions: {
+    test: function() {
+      this.set('editMode', true);
+      this.get('currentPoses').set('[]');
+      let as = this.get('model.asanasequences');
+
+      as.forEach((as, index, array) => {
+        let a = {
+          id: as.id,
+          sortOrder: as.get('sortOrder'),
+          sequenceID: as.get('sequence.id'),
+          asana: as.get('asana.content.data')
+        }
+        this.get('currentPoses').pushObject(a);
+      })
+      console.log(this.get('currentPoses'));
+    }
+  }
+
 });
